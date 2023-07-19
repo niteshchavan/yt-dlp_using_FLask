1110af6b8fec96d55da93079e454f72e58a45657
Nitesh Chavan <76774483+niteshchavan@users.noreply.github.com>
2023-07-19 21:49:28 +0530

    Update yt-dlp.py

--- yt-dlp.py
+++ yt-dlp.py
@@ -8,18 +8,33 @@ app = Flask(__name__)
 def download_video(video_url, download_dir):
     import yt_dlp
 
-    ydl_opts = {
+    ydl_opts_720p = {
         'outtmpl': os.path.join(download_dir, '%(title)s.%(ext)s'),
+        'format': '22',  # Format 22 represents 720p resolution
     }
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl_opts_360p = {
+        'outtmpl': os.path.join(download_dir, '%(title)s.%(ext)s'),
+        'format': '18',  # Format 18 represents 360p resolution
+    }
+
+    # First, try to download in 720p resolution (format 22)
+    with yt_dlp.YoutubeDL(ydl_opts_720p) as ydl:
         try:
             info_dict = ydl.extract_info(video_url, download=True)
             video_title = info_dict.get('title', 'Unknown')
             return video_title
-        except yt_dlp.DownloadError as e:
-            print(f"Error: {str(e)}")
-            return None
+        except yt_dlp.DownloadError:
+            # If 720p format is not available, fallback to 360p resolution (format 18)
+            with yt_dlp.YoutubeDL(ydl_opts_360p) as ydl:
+                try:
+                    info_dict = ydl.extract_info(video_url, download=True)
+                    video_title = info_dict.get('title', 'Unknown')
+                    return video_title
+                except yt_dlp.DownloadError as e:
+                    print(f"Error: {str(e)}")
+                    return None
+
 
 @app.route('/')
 def index():
